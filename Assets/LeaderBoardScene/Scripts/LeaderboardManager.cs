31f9bd4270e756725876ce1718f90eeb0a54a68c
A01723266 <A01723266@tec.mx>
2025-04-29 23:56:59 -0600

    .

--- Assets/LeaderBoardScene/Scripts/LeaderboardManager.cs
+++ Assets/LeaderBoardScene/Scripts/LeaderboardManager.cs
@@ -1,7 +1,5 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System;
-using UnityEditor.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking;
